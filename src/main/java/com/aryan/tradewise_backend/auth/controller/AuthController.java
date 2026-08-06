fedc7748a1d96f85494886a09d3b35784fa764f5
Aryan Saini <aaryansaini2005@gmail.com>
2026-08-06 13:51:33 +0530

    AuthController implemented

---
+++ src/main/java/com/aryan/tradewise_backend/auth/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.aryan.tradewise_backend.auth.controller;
+
+import com.aryan.tradewise_backend.auth.dto.RegisterRequest;
+import com.aryan.tradewise_backend.auth.dto.RegisterResponse;
+import com.aryan.tradewise_backend.auth.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
+        RegisterResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+
+
+}
